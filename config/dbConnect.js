c7826b2b5d21e3c3c02b0b93588449a18e1a5717
Nathan <nathansilva406@gmail.com>
2023-10-25 13:03:33 -0300

    conexão com o banco

---
+++ config/dbConnect.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+async function conectaDatabase() {
+    mongoose.connect(process.env.DB_CONNECTION_STRING)
+    return mongoose.connection
+}
+
+export default conectaDatabase
