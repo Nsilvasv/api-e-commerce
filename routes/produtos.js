ea30e29dd476e4a13d9dd6c7b283f7beec89da99
Nathan <nathansilva406@gmail.com>
2023-10-26 13:18:36 -0300

    configurando rotas

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,38 +1,12 @@
 import { Router } from "express";
-import { produtos } from "../data.js";
+import produtoController from "../controllers/produto.js";
 
 const router = Router()
 
-function buscaProdutos(id) {
-    return produtos.findIndex(produto => {
-        return produto.id === Number(id)
-    })
-}
+router.get("/produtos", produtoController.getProdutos)
 
-router.get("/produtos", (req, res) => {
-    res.status(200).json(produtos)
-})
+router.get("/produtos/:id", produtoController.getProdutosId)
 
-router.get("/produtos/:id", (req, res) => {
-    const index = buscaProdutos(req.params.id)
-    res.status(200).json(produtos[index])
-})
-
-router.post("/produtos", (req, res) => {
-    produtos.push(req.body)
-    res.send("produto inserido com sucesso!")
-})
-
-router.put("/produtos/:id", (req, res) => {
-    const index = buscaProdutos(req.params.id)
-    produtos[index].body = req.body
-    res.send("Update concluído!")
-})
-
-router.delete("/produtos/:id", (req, res) => {
-    const index = buscaProdutos(req.params.id)
-    produtos.splice(index, 1)
-    res.status(201).send("Deletado")
-})
+router.post("/produtos", produtoController.postProdutos)
 
 export default router
