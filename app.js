729d39ff8bc1807a2c4c8c8b0fa200e99551136a
Nathan <nathansilva406@gmail.com>
2023-10-26 15:35:38 -0300

    arquivo raiz

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import express from "express";
 import router from "./routes/produtos.js";
+import conectaDatabase from "./config/dbConnect.js";
+import "dotenv/config.js"
+
+const conexao = await conectaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso");
+})
 
 const app = express()
+app.use(express.json())
 
 app.use("/", router)
+app.use("/produtos", router)
 
 app.listen(3000, () => {
     console.log("server runing");
